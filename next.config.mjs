92e6114c0676ad3c6ec839e0aa85272ad5d1074d
Jack Clackett <jack@noquarter.co>
2024-05-17 14:33:16 +0200

    ignore lint and ts

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true },
+}
 
-export default nextConfig;
+export default nextConfig
